eaa543b802d9dd22505c9db5d688c923de32630d
conwabueze <chinonso1nwabueze@gmail.com>
2019-01-06 20:39:59 -0500

    button on click working

--- src/components/QuoteComponent.js
+++ src/components/QuoteComponent.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import {Button} from 'reactstrap'
+import {QUOTEINFO} from '../shared/quoteInfo';
 
 class Quote extends React.Component{
     //constructor
     constructor(props){
         super(props);
+
+        this.state={
+            quoteInfo: []
+        }
+    }
+
+    componentDidMount(){
+        this.getQuote();
+    };
+
+    handleClick(){
+        this.getQuote();
+    };
+
+    //method used to generate a new quote
+    getQuote(){
+        //varible generates random number from 1 to the length of state file
+        const randomNumber = Math.floor((Math.random()*QUOTEINFO.length)+1);
+       
+        const quoteDetail = QUOTEINFO.map(quote=>{       
+                if(quote.id===randomNumber){
+                    this.setState({quoteInfo: quote});
+                }
+            }
+        );
     }
     
     //render method
     render(){
         return(
-            <div>{this.props.text}</div>
-            
+            <div>
+                <p>{this.state.quoteInfo.text}</p>
+                <Button block onClick={this.getQuote.bind(this)}>CLICK FOR NEW QUOTE</Button>
+            </div>
         );
     };
     
--- src/index.js
+++ src/index.js
@@ -1,59 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {QUOTEINFO} from './shared/quoteInfo';
 import Quote from './components/QuoteComponent';
 
 
-class App extends React.Component{
-    //constructor
-    constructor(props){
-        super(props);
-
-        this.state = {
-            quotes: QUOTEINFO,
-            
-        };
-    }
-
-    //method used to get details and returns quote to be rendered in render method
-    getQuote(){
-        //varible generates random number from 1 to the length of state file
-        const randomNumber = Math.floor((Math.random()*this.state.quotes.length)+1);
-       
-        /*
-         const target = (number) =>{
-            return number === this.state.quotes.id;
-        }
-        first attempt using filter method
-        const quoteDetail = this.state.quotes.filter(() =>{
-            console.log(this.state.quotes.quote);
-            return randomNumber === this.state.quotes.id;
-        })[0];
-
-        console.log(quoteDetail);
-        */
-        const quoteDetail = this.state.quotes.map(quote=>{
-            
-                if(quote.id===randomNumber){
-                    return(
-                        <Quote text={quote.text}/>
-                    );
-                    
-                }
-            }
-        );
-
-      
-        
-    }
-
+class App extends React.Component{s
     //render method
     render(){
-    
         return(
-            
-            <div>{this.getQuote()}</div>
+            <Quote />
         );
     }
 }
